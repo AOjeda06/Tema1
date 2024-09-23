6031f4e453c78a6b3cda20b4bef8189761e82ab5
andres.ojeda <andres.ojeda@106-24.info2>
2024-09-23 12:44:02 +0200

    update ej5

--- Tema 1/src/parte1/Ej5.java
+++ Tema 1/src/parte1/Ej5.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Ej5 {
 	public static void main(String[] args) {
-		float radio, area, longitud; //declaramos las variables
+		double radio, area, longitud; //declaramos las variables
 		Scanner sc = new Scanner(System.in); //configuramos el scanner
 		System.out.println("Introduzca el radio de su circunferencia para calcular su area y longitud: "); //pide los datos por pantalla
-		radio = sc.nextFloat(); //lee los datos por teclado
-		System.out.println("El área es: " + (radio*radio*Math.PI) + " y la longitud es: " + (radio*2*Math.PI)); //calcula y devuelve la respuesta
+		radio = sc.nextDouble(); //lee los datos por teclado
+		area = (radio*radio*Math.PI); //calcula el area
+		longitud = (radio*2*Math.PI); //calcula la longitud
+		System.out.println("El área es: " + area + " y la longitud es: " + longitud); //devuelve la respuesta
 	}
 }
